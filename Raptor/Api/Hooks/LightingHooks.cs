5e64ab2cad071a985a3b243d06dc6ad7e0a034c1
MarioE <MarioE27@hotmail.com>
2013-11-12 21:45:29 -0500

    Add lighting hooks

---
+++ Raptor/Api/Hooks/LightingHooks.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Terraria;
+
+namespace Raptor.Api.Hooks
+{
+	/// <summary>
+	/// The API's lighting hooks.
+	/// </summary>
+	public static class LightingHooks
+	{
+		#region Color
+		/// <summary>
+		/// Occurs before the tiles are colored.
+		/// </summary>
+		public static event EventHandler Color;
+		internal static void InvokeColor()
+		{
+			if (Color != null)
+				Color(null, EventArgs.Empty);
+		}
+		#endregion
+
+		#region ColorR
+		/// <summary>
+		/// Event arguments for Color-related hooks.
+		/// </summary>
+		public class ColorEventArgs : HandledEventArgs
+		{
+			/// <summary>
+			/// Gets the X position of the tile.
+			/// </summary>
+			public int X { get; private set; }
+			/// <summary>
+			/// Gets the Y position of the tile.
+			/// </summary>
+			public int Y { get; private set; }
+			internal ColorEventArgs(int x, int y)
+			{
+				X = x;
+				Y = y;
+			}
+		}
+		/// <summary>
+		/// Occurs before a tile is colored red.
+		/// </summary>
+		public static event EventHandler<ColorEventArgs> ColorR;
+		internal static bool InvokeColorR(int x, int y)
+		{
+			if (ColorR != null)
+			{
+				var args = new ColorEventArgs(x, y);
+				ColorR(null, args);
+				return args.Handled;
+			}
+			return false;
+		}
+		#endregion
+		#region ColorG
+		/// <summary>
+		/// Occurs before a tile is colored green.
+		/// </summary>
+		public static event EventHandler<ColorEventArgs> ColorG;
+		internal static bool InvokeColorG(int x, int y)
+		{
+			if (ColorG != null)
+			{
+				var args = new ColorEventArgs(x, y);
+				ColorG(null, args);
+				return args.Handled;
+			}
+			return false;
+		}
+		#endregion
+		#region ColorB
+		/// <summary>
+		/// Occurs before a tile is colored blue.
+		/// </summary>
+		public static event EventHandler<ColorEventArgs> ColorB;
+		internal static bool InvokeColorB(int x, int y)
+		{
+			if (ColorB != null)
+			{
+				var args = new ColorEventArgs(x, y);
+				ColorB(null, args);
+				return args.Handled;
+			}
+			return false;
+		}
+		#endregion
+	}
+}
--- Raptor/Program.cs
+++ Raptor/Program.cs
@@ -129,6 +129,63 @@ namespace Raptor
 					Instruction.Create(OpCodes.Ret));
 			}
 			#endregion
+			#region Lighting
+			{
+				var doColors = asm.GetMethod("Lighting", "doColors");
+				for (int i = doColors.Body.Instructions.Count - 1; i >= 0; i--)
+				{
+					var instr = doColors.Body.Instructions[i];
+					if (instr.OpCode == OpCodes.Callvirt && ((MethodReference)instr.Operand).Name == "Restart")
+					{
+						doColors.InsertAfter(instr,
+							Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColor", FLAGS))));
+					}
+				}
+
+				var colorR = asm.GetMethod("Lighting", "LightColor");
+				colorR.InsertStart(
+					Instruction.Create(OpCodes.Ldarg_0),
+					Instruction.Create(OpCodes.Ldarg_1),
+					Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColorR", FLAGS))),
+					Instruction.Create(OpCodes.Brfalse_S, colorR.Body.Instructions[0]),
+					Instruction.Create(OpCodes.Ret));
+				var colorR2 = asm.GetMethod("Lighting", "LightColorG");
+				colorR2.InsertStart(
+					Instruction.Create(OpCodes.Ldarg_0),
+					Instruction.Create(OpCodes.Ldarg_1),
+					Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColorR", FLAGS))),
+					Instruction.Create(OpCodes.Brfalse_S, colorR2.Body.Instructions[0]),
+					Instruction.Create(OpCodes.Ret));
+				var colorG = asm.GetMethod("Lighting", "LightColorG");
+				colorG.InsertStart(
+					Instruction.Create(OpCodes.Ldarg_0),
+					Instruction.Create(OpCodes.Ldarg_1),
+					Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColorG", FLAGS))),
+					Instruction.Create(OpCodes.Brfalse_S, colorG.Body.Instructions[0]),
+					Instruction.Create(OpCodes.Ret));
+				var colorG2 = asm.GetMethod("Lighting", "LightColorG2");
+				colorG2.InsertStart(
+					Instruction.Create(OpCodes.Ldarg_0),
+					Instruction.Create(OpCodes.Ldarg_1),
+					Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColorG", FLAGS))),
+					Instruction.Create(OpCodes.Brfalse_S, colorG2.Body.Instructions[0]),
+					Instruction.Create(OpCodes.Ret));
+				var colorB = asm.GetMethod("Lighting", "LightColorB");
+				colorB.InsertStart(
+					Instruction.Create(OpCodes.Ldarg_0),
+					Instruction.Create(OpCodes.Ldarg_1),
+					Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColorB", FLAGS))),
+					Instruction.Create(OpCodes.Brfalse_S, colorB.Body.Instructions[0]),
+					Instruction.Create(OpCodes.Ret));
+				var colorB2 = asm.GetMethod("Lighting", "LightColorB2");
+				colorB2.InsertStart(
+					Instruction.Create(OpCodes.Ldarg_0),
+					Instruction.Create(OpCodes.Ldarg_1),
+					Instruction.Create(OpCodes.Call, mod.Import(typeof(LightingHooks).GetMethod("InvokeColorB", FLAGS))),
+					Instruction.Create(OpCodes.Brfalse_S, colorB2.Body.Instructions[0]),
+					Instruction.Create(OpCodes.Ret));
+			}
+			#endregion
 			#region Main
 			{
 				var draw = asm.GetMethod("Main", "Draw");
@@ -410,7 +467,7 @@ namespace Raptor
 				foreach (var method in type.Methods)
 					method.IsPublic = true;
 			}
-			
+
 			Log.Initialize();
 			ClientApi.Initialize();
 			GameHooks.InvokeILModified(asm);
